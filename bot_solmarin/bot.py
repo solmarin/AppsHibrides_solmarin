97e1a6d25ce6da55d626b1a860da270bb0dbf0f7
solmarin <55603215+solmarin@users.noreply.github.com>
2019-10-23 18:06:20 +0200

    Add files via upload

--- bot_solmarin/bot.py
+++ bot_solmarin/bot.py
@@ -1,28 +1,28 @@
 # -*- coding: utf-8 -*-
-
 import telebot
-respuesta = False
+global respuesta
+respuesta = True
 bot = telebot.TeleBot("961311462:AAHxlkA4pjGnQrc2faCXdYCmGQo4tvOjJk0")
 
-print respuesta
 @bot.message_handler(commands=['start', 'hola'])
 def send_welcome(message):
-    bot.reply_to(message, "Hola! Queria preguntarte.. conoces a LLuc? si/no")
+    global respuesta
     respuesta = True
-print respuesta
+    bot.reply_to(message, "Hola! Queria preguntarte.. conoces a LLuc? si/no")
+
 @bot.message_handler(content_types=['text'])
-def respostas(message,respuesta=respuesta):
+def respostas(message):
+    global respuesta
     if respuesta:
         if message.text == "si":
             respuesta = False
-            bot.send_message("Si? Felicidades, ya puedes decir que conoces las respuestas a todo!")
-
+            bot.send_message(message.chat.id,"Si? Felicidades, ya puedes decir que conoces las respuestas a todo!")
         elif message.text == "no":
             respuesta = False
-            bot.send_message("En serio? Entonces no sabes lo que es la belleza.")
+            bot.send_message(message.chat.id,"En serio? Entonces no sabes lo que es la belleza.")
         else:
             respuesta = False
-            bot.send_message("No entiendo tu respuesta, mira esto:")
+            bot.send_message(message.chat.id,"No entiendo tu respuesta, mira esto:")
 
 
 bot.polling(none_stop=True)
