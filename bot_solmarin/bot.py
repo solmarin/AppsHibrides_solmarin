775a8de7e15eb00feb3b7544d0a0fb7b2a32d21a
solmarin <55603215+solmarin@users.noreply.github.com>
2019-10-30 18:12:00 +0100

    <version 2>

--- bot_solmarin/bot.py
+++ bot_solmarin/bot.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 import telebot
+import random
 global respuesta
-respuesta = True
+respuesta = False
 bot = telebot.TeleBot("961311462:AAHxlkA4pjGnQrc2faCXdYCmGQo4tvOjJk0")
-
 @bot.message_handler(commands=['start', 'hola'])
 def send_welcome(message):
     global respuesta
     respuesta = True
     bot.reply_to(message, "Hola! Queria preguntarte.. conoces a LLuc? si/no")
+
+@bot.message_handler(commands=['random'])
+def photoRandom(message):
+    i = (random.randint(1,5))
+    switcher = {
+        1: bot.send_photo(message.chat.id,open('imagenes/1.png', 'rb')),
+        2: bot.send_photo(message.chat.id,open('imagenes/2.png', 'rb')),
+        3: bot.send_photo(message.chat.id,open('imagenes/3.png', 'rb')),
+        4: bot.send_photo(message.chat.id,open('imagenes/4.png', 'rb')),
+        5: bot.send_photo(message.chat.id,open('imagenes/5.png', 'rb'))
+    }
+    bot.reply_to(message,switcher.get(i,"PROBLEM RANDOM"))
 
 @bot.message_handler(content_types=['text'])
 def respostas(message):
@@ -23,6 +35,8 @@ def respostas(message):
         else:
             respuesta = False
             bot.send_message(message.chat.id,"No entiendo tu respuesta, mira esto:")
+
+
 
 
 bot.polling(none_stop=True)
